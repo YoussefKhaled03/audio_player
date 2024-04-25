eeef34b587fdc9e89ef4fab50d2802201503ab71
mohamedabououf <mohabououf@gmail.com>
2024-04-25 03:06:36 +0200

    Update main.py with new window title and size

--- '''simple media player using tkinter'''.py
+++ '''simple media player using tkinter'''.py
@@ -11,36 +11,28 @@ class MediaPlayer:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title('foxi')
-        self.root.geometry('800x400')
+        self.root.geometry('300x100')
         self.root.resizable(True, True)
         self.frame = ttk.Frame(self.root)
+        self.frame.pack()  # pack the frame into the window
         self.create_widgets()
         self.player = vlc.MediaPlayer()
-        image_icon = tk.PhotoImage(file = "foxi22.png")
+        image_icon = tk.PhotoImage(file = "foxi.JPG")
         self.root.iconphoto(False, image_icon)
-        lower_frame = tk.Frame(self.root, bg='#09edff')
-        lower_frame.pack(side='bottom', fill='both', expand=True)
-
 
     def create_widgets(self):
         self.play_button = ttk.Button(self.root, text='Play', command=self.play)
         self.play_button.pack()
         self.stop_button = ttk.Button(self.root, text='Stop', command=self.stop)
         self.stop_button.pack()
-        self.open_button = ttk.Button(self.root, text='Open', command=self.open)
-        self.open_button.pack()
 
     def play(self):
-        self.player.play()
+        # Add code to play media
+        pass
 
     def stop(self):
-        self.player.stop()
+        # Add code to stop media
+        pass
 
-    def open(self):
-        filepath = filedialog.askopenfilename()
-        if filepath:
-            self.player.set_mrl(filepath)
-
-# Create an instance of the media player and start the Tkinter event loop
-media_player = MediaPlayer()
-media_player.root.mainloop()
+app = MediaPlayer()  # create an instance of the application
+app.root.mainloop()  # run the application
